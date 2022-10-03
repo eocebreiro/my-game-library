f69805554f55c72460711738171ac3de111c0dc0
Eric <eocebreiro@gmail.com>
2022-10-03 23:19:20 +0900

    first commit

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Routes
+
+const cors = require("cors");
+
+//Init Middleware
+app.use(morgan("dev"));
+app.use(express.json({ extended: false }));
+
+//Connect to the database
+const { MONGOURI } = process.env;
+try {
+  mongoose.connect(MONGOURI, { useNewUrlParser: true });
+  console.log("MongoDB Connected...");
+} catch (err) {
+  console.error(err.message);
+  // Exit process with failure
+  process.exit(1);
+}
+
+app.use(cors());
+
+//Routes which should handle requests
+app.get("/", (req, res) => res.send("Welcome to My Gaming Library"));
+
+// Error 404
+app.use((req, res, next) => {
+  const error = new Error("Not Found (404)");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;

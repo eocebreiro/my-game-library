2ad4e0515563524ebf694943c04127932829e009
Eric <eocebreiro@gmail.com>
2022-10-10 01:57:57 +0900

    added a private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -11,23 +12,33 @@ import { setAuthToken } from "./utils/setAuthToken";
 import { loadUser } from "./contexts/UserActions";
 
 // Components
+import { PrivateRoute } from "./components/PrivateRoute";
 import { Landing } from "./layout/Landing";
 import { Dashboard } from "./layout/Dashboard";
-
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
 
 function App() {
   const { dispatch } = useUser();
 
   useEffect(() => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
     loadUser(dispatch);
   }, []);
 
   return (
     <div className="App bg-dark">
-      <Landing />
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

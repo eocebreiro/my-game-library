e7f08101054cff14ef7a1f7a6b727644505c22e7
Eric <eocebreiro@gmail.com>
2022-10-10 22:09:02 +0900

    created the dashboard layout

--- client/src/layout/Dashboard.js
+++ client/src/layout/Dashboard.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="container">
+      <div className="row gap-2">
+        <div className="card">
+          <div>Unfinished</div>
+          <div>Beaten</div>
+          <div>Completed</div>
+        </div>
+        <div className="card">
+          <div>Filter</div>
+        </div>
+        <div className="card">
+          <div>LIST OF GAMES</div>
+        </div>
+      </div>
+    </div>
+  );
 };

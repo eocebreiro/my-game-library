72623219872c32089a6d58c2316737e9b433a8cf
Eric <eocebreiro@gmail.com>
2022-10-12 01:48:13 +0900

    organized code to look better

--- client/src/layout/AddGame.js
+++ client/src/layout/AddGame.js
@@ -1,50 +1,57 @@
 import React, { useState } from "react";
 
 export const AddGame = () => {
-  // State for the raw form data input from the user
-  const [formData, setFormData] = useState({
-    name: "",
-    compilation: "",
-    system: "",
-    status: "",
-    ownership: "",
-    hours: "",
-    rating: "",
-    comments: "",
+  const [name, setName] = useState({
+    field: "",
+    feedback: "",
+    error: true,
   });
 
-  const {
-    name,
-    compilation,
-    system,
-    status,
-    ownership,
-    hours,
-    rating,
-    comments,
-  } = formData;
-
-  // State for error feedback
-  const [formFeedback, setFormFeedback] = useState({
-    nameFB: "",
-    systemFB: "",
-    statusFB: "",
-    ownershipFB: "",
-    hoursFB: "",
-    ratingFB: "",
+  const [compilation, setCompilation] = useState({
+    field: "", // This is optional
   });
 
-  const { nameFB, systemFB, statusFB, ownershipFB, hoursFB, ratingFB } =
-    formFeedback;
-
-  // State for undisabling items in form depending on what is chosen for status
-  const [formDisable, setFormDisable] = useState({
-    ownershipDisable: true,
-    hoursDisable: true,
-    ratingDisable: true,
+  const [system, setSystem] = useState({
+    field: "",
+    feedback: "",
+    error: true,
   });
 
-  const { ownershipDisable, hoursDisable, ratingDisable } = formDisable;
+  const [status, setStatus] = useState({
+    field: "",
+    feedback: "",
+    error: true,
+  });
+
+  const [ownership, setOwnership] = useState({
+    field: "",
+    disable: true,
+    feedback: "",
+    error: true,
+  });
+
+  const [hours, setHours] = useState({
+    field: "",
+    disable: true,
+    feedback: "",
+    error: true,
+  });
+
+  const [rating, setRating] = useState({
+    field: "",
+    disable: true,
+    feedback: "",
+    error: true,
+  });
+
+  const [review, setReview] = useState({
+    field: "", // This is optional
+    disable: true,
+  });
+
+  const [comments, setComments] = useState({
+    field: "", // This is optional
+  });
 
   const statusOptions = [
     <option key="1">Select an option</option>,
@@ -67,203 +74,263 @@ export const AddGame = () => {
 
   // Check validation while user is inputing
   const onChange = (e) => {
-    // Disable or Undisable form items depending on selection of status
-    if (e.target.name === "status") {
-      if (
-        e.target.value === "Unfinished" ||
-        e.target.value === "Beaten" ||
-        e.target.value === "Completed"
-      ) {
-        setFormDisable({
-          ...formDisable,
-          ownershipDisable: false,
-          hoursDisable: false,
-          ratingDisable: false,
-        });
-      } else if (e.target.value === "Backlog") {
-        setFormDisable({
-          ...formDisable,
-          ownershipDisable: false,
-          hoursDisable: true,
-          ratingDisable: true,
-        });
-      } else if (
-        e.target.value === "Wishlist" ||
-        e.target.value === "Select an option"
-      ) {
-        setFormDisable({
-          ...formDisable,
-          ownershipDisable: true,
-          hoursDisable: true,
-          ratingDisable: true,
-        });
-      }
-    }
-
-    // Check name
+    // Check Name Validation
     if (e.target.name === "name") {
       if (e.target.value === "") {
-        setFormFeedback({ ...formFeedback, nameFB: "is-invalid" });
+        setName({
+          ...name,
+          field: e.target.value,
+          feedback: "is-invalid",
+          error: true,
+        });
       } else {
-        setFormFeedback({ ...formFeedback, nameFB: "is-valid" });
+        setName({
+          ...name,
+          field: e.target.value,
+          feedback: "is-valid",
+          error: false,
+        });
       }
     }
 
-    // Check system
+    // Check Compilation Validation
+    if (e.target.name === "compilation") {
+      if (e.target.value === "") {
+        setCompilation({
+          ...compilation,
+          field: e.target.value,
+          feedback: "",
+        });
+      } else {
+        setCompilation({
+          ...compilation,
+          field: e.target.value,
+          feedback: "is-valid",
+        });
+      }
+    }
+
+    // Check System Validation
     if (e.target.name === "system") {
       if (e.target.value === "") {
-        setFormFeedback({ ...formFeedback, systemFB: "is-invalid" });
+        setSystem({
+          ...system,
+          field: e.target.value,
+          feedback: "is-invalid",
+          error: true,
+        });
       } else {
-        setFormFeedback({ ...formFeedback, systemFB: "is-valid" });
+        setSystem({
+          ...system,
+          field: e.target.value,
+          feedback: "is-valid",
+          error: false,
+        });
       }
     }
 
-    // Check status and clear feedback on disable items
+    // Check Status Validation
     if (e.target.name === "status") {
-      if (e.target.value === "Select an option") {
-        setFormFeedback({
-          ...formFeedback,
-          statusFB: "is-invalid",
-          ownershipFB: "",
-          hoursFB: "",
-          ratingFB: "",
+      if (e.target.value === "" || e.target.value === "Select an option") {
+        setStatus({
+          ...status,
+          field: e.target.value,
+          feedback: "is-invalid",
+          error: true,
         });
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-          ownership: "",
-          hours: "",
-          rating: "",
+      } else {
+        setStatus({
+          ...status,
+          field: e.target.value,
+          feedback: "is-valid",
+          error: false,
         });
-        return;
+      }
+      /** The next part of validation depends on what is selected in status */
+      if (
+        e.target.value === "Select an option" ||
+        e.target.value === "Wishlist"
+      ) {
+        setOwnership({ ...ownership, disable: true });
+        setHours({ ...hours, disable: true });
+        setRating({ ...rating, disable: true });
+        setReview({ ...review, disable: true });
       } else if (e.target.value === "Backlog") {
-        setFormFeedback({
-          ...formFeedback,
-          hoursFB: "",
-          ratingFB: "",
-        });
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-          hours: "",
-          rating: "",
-        });
-        return;
-      } else if (e.target.value === "Wishlist") {
-        setFormFeedback({
-          ...formFeedback,
-          ownershipFB: "",
-          hoursFB: "",
-          ratingFB: "",
-        });
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-          ownership: "",
-          hours: "",
-          rating: "",
-        });
-        return;
+        setOwnership({ ...ownership, disable: false });
+        setHours({ ...hours, disable: true });
+        setRating({ ...rating, disable: true });
+        setReview({ ...review, disable: true });
       } else {
-        setFormFeedback({ ...formFeedback, statusFB: "is-valid" });
+        setOwnership({ ...ownership, disable: false });
+        setHours({ ...hours, disable: false });
+        setRating({ ...rating, disable: false });
+        setReview({ ...review, disable: false });
       }
     }
-    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    // Check ownership
+    // Check Ownership Validation
     if (e.target.name === "ownership") {
-      if (e.target.value === "Select an option") {
-        setFormFeedback({ ...formFeedback, ownershipFB: "is-invalid" });
+      if (e.target.value === "" || e.target.value === "Select an option") {
+        setOwnership({
+          ...ownership,
+          field: e.target.value,
+          feedback: "is-invalid",
+          error: true,
+        });
       } else {
-        setFormFeedback({ ...formFeedback, ownershipFB: "is-valid" });
+        setOwnership({
+          ...ownership,
+          field: e.target.value,
+          feedback: "is-valid",
+          error: false,
+        });
       }
     }
 
-    // Check hours
+    // Check Hours Validation
     if (e.target.name === "hours") {
       if (
         e.target.value === "" ||
         isNaN(e.target.value) ||
         e.target.value < 0
       ) {
-        setFormFeedback({ ...formFeedback, hoursFB: "is-invalid" });
+        setHours({
+          ...hours,
+          field: e.target.value,
+          feedback: "is-invalid",
+          error: true,
+        });
       } else {
-        setFormFeedback({ ...formFeedback, hoursFB: "is-valid" });
+        setHours({
+          ...hours,
+          field: e.target.value,
+          feedback: "is-valid",
+          error: false,
+        });
       }
     }
 
-    // Check rating
+    // Check Rating Validation
     if (e.target.name === "rating") {
       if (
         e.target.value === "" ||
         isNaN(e.target.value) ||
-        (e.target.value < 0 && e.target.value > 10)
+        e.target.value < 0 ||
+        e.target.value > 10
       ) {
-        setFormFeedback({ ...formFeedback, ratingFB: "is-invalid" });
+        setRating({
+          ...rating,
+          field: e.target.value,
+          feedback: "is-invalid",
+          error: true,
+        });
       } else {
-        setFormFeedback({ ...formFeedback, ratingFB: "is-valid" });
+        setRating({
+          ...rating,
+          field: e.target.value,
+          feedback: "is-valid",
+          error: false,
+        });
+      }
+    }
+
+    // Check Review Validation
+    if (e.target.name === "review") {
+      if (e.target.value === "") {
+        setReview({
+          ...review,
+          field: e.target.value,
+          feedback: "",
+        });
+      } else {
+        setReview({ ...review, field: e.target.value, feedback: "is-valid" });
+      }
+    }
+
+    // Check Comments Validation
+    if (e.target.name === "comments") {
+      if (e.target.value === "") {
+        setComments({ ...comments, field: e.target.value, feedback: "" });
+      } else {
+        setComments({
+          ...comments,
+          field: e.target.value,
+          feedback: "is-valid",
+        });
       }
     }
   };
 
-  // Check validation before the user submits
   const onSubmit = async (e) => {
     e.preventDefault();
-    let isError = false;
-    let validation = {
-      nameFB: nameFB,
-      systemFB: systemFB,
-      statusFB: statusFB,
-      ownershipFB: ownershipFB,
-      hoursFB: hoursFB,
-      ratingFB: ratingFB,
-    };
 
-    if (nameFB !== "is-valid") {
-      validation.nameFB = "is-invalid";
-      isError = true;
+    // Check for errors and if so, display the errors for the user
+    if (name.error) {
+      setName({ ...name, feedback: "is-invalid" });
     }
-    if (systemFB !== "is-valid") {
-      validation.systemFB = "is-invalid";
-      isError = true;
+    if (system.error) {
+      setSystem({ ...system, feedback: "is-invalid" });
     }
-    if (statusFB !== "is-valid") {
-      validation.statusFB = "is-invalid";
-      isError = true;
+    if (status.error) {
+      setStatus({ ...status, feedback: "is-invalid" });
+    }
+    if (!ownership.disable && ownership.error) {
+      setOwnership({ ...ownership, feedback: "is-invalid" });
+    }
+    if (!hours.disable && hours.error) {
+      setHours({ ...hours, feedback: "is-invalid" });
+    }
+    if (!rating.disable && rating.error) {
+      setRating({ ...rating, feedback: "is-invalid" });
     }
 
-    // Check feedback of items depending on option selected in status
-    if (!ownershipDisable) {
-      if (ownershipFB !== "is-valid") {
-        validation.ownershipFB = "is-invalid";
-        isError = true;
-      }
-    }
-    if (!hoursDisable) {
-      if (hoursFB !== "is-valid") {
-        validation.hoursFB = "is-invalid";
-        isError = true;
-      }
-    }
-    if (!ratingDisable) {
-      if (ratingFB !== "is-valid") {
-        validation.ratingFB = "is-invalid";
-        isError = true;
-      }
-    }
+    if (
+      !name.error &&
+      !system.error &&
+      !status.error &&
+      (ownership.disable || (!ownership.error && !ownership.disable)) &&
+      (hours.disable || (!hours.error && !hours.disable)) &&
+      (rating.disable || (!rating.error && !rating.disable))
+    ) {
+      // if no errors, then build object to send
+      let results = {
+        name: name.field,
+        compilation: null,
+        system: system.field,
+        status: status.field,
+        ownership: null,
+        hours: null,
+        rating: null,
+        review: null,
+        comments: null,
+      };
 
-    if (isError) {
-      setFormFeedback({
-        ...formFeedback,
-        nameFB: validation.nameFB,
-        systemFB: validation.systemFB,
-        statusFB: validation.statusFB,
-        ownershipFB: validation.ownershipFB,
-        hoursFB: validation.hoursFB,
-        ratingFB: validation.ratingFB,
-      });
-    } else {
-      console.log("it works!");
+      if (compilation.field !== "") {
+        results["compilation"] = compilation.field;
+      }
+
+      if (!ownership.disable && !ownership.error) {
+        results["ownership"] = ownership.field;
+      }
+
+      if (!hours.disable && !hours.error) {
+        results["hours"] = hours.field;
+      }
+
+      if (!rating.disable && !rating.error) {
+        results["rating"] = rating.field;
+      }
+
+      if (!review.disable && review.field !== "") {
+        results["review"] = review.field;
+      }
+
+      if (comments.field !== "") {
+        results["comments"] = comments.field;
+      }
+
+      // Send Successful result
+      console.log(results);
     }
   };
 
@@ -281,9 +348,9 @@ export const AddGame = () => {
                   <div className="form-outline">
                     Name:{" "}
                     <input
-                      className={`form-control ${nameFB}`}
+                      className={`form-control ${name.feedback}`}
                       name="name"
-                      value={name}
+                      value={name.field}
                       onChange={(e) => onChange(e)}
                     />
                     <div className="invalid-feedback">Name is required.</div>
@@ -292,9 +359,9 @@ export const AddGame = () => {
                 <div className="col-md-6 mb-4">
                   Compilation:{" "}
                   <input
-                    className="form-control"
+                    className={`form-control ${compilation.feedback}`}
                     name="compilation"
-                    value={compilation}
+                    value={compilation.field}
                     onChange={(e) => onChange(e)}
                   />
                 </div>
@@ -304,9 +371,9 @@ export const AddGame = () => {
                   {" "}
                   System:{" "}
                   <input
-                    className={`form-control ${systemFB}`}
+                    className={`form-control ${system.feedback}`}
                     name="system"
-                    value={system}
+                    value={system.field}
                     onChange={(e) => onChange(e)}
                   />
                   <div className="invalid-feedback">System is required.</div>
@@ -314,9 +381,9 @@ export const AddGame = () => {
                 <div className="col-md-6 mb-4">
                   Status:{" "}
                   <select
-                    className={`form-control ${statusFB}`}
+                    className={`form-control ${status.feedback}`}
                     name="status"
-                    value={status}
+                    value={status.field}
                     onChange={(e) => onChange(e)}
                   >
                     {statusOptions}{" "}
@@ -328,11 +395,13 @@ export const AddGame = () => {
                 <div className="col-md-4 mb-4">
                   Ownership:{" "}
                   <select
-                    className={`form-control ${ownershipFB}`}
+                    className={`form-control ${
+                      ownership.disable ? "" : ownership.feedback
+                    }`}
                     name="ownership"
-                    value={ownership}
+                    value={ownership.disable ? "" : ownership.field}
                     onChange={(e) => onChange(e)}
-                    disabled={ownershipDisable}
+                    disabled={ownership.disable}
                   >
                     {ownershipOptions}
                   </select>
@@ -341,10 +410,12 @@ export const AddGame = () => {
                 <div className="col-md-4 mb-4">
                   Hours Played:{" "}
                   <input
-                    className={`form-control ${hoursFB}`}
-                    disabled={hoursDisable}
+                    className={`form-control ${
+                      hours.disable ? "" : hours.feedback
+                    }`}
+                    disabled={hours.disable}
                     name="hours"
-                    value={hours}
+                    value={hours.disable ? "" : hours.field}
                     onChange={(e) => onChange(e)}
                   />
                   <div className="invalid-feedback">
@@ -354,10 +425,12 @@ export const AddGame = () => {
                 <div className="col-md-4 mb-4">
                   Rating:{" "}
                   <input
-                    className={`form-control ${ratingFB}`}
-                    disabled={ratingDisable}
+                    className={`form-control ${
+                      rating.disable ? "" : rating.feedback
+                    }`}
+                    disabled={rating.disable}
                     name="rating"
-                    value={rating}
+                    value={rating.disable ? "" : rating.field}
                     onChange={(e) => onChange(e)}
                   />
                   <div className="invalid-feedback">
@@ -366,15 +439,29 @@ export const AddGame = () => {
                 </div>
               </div>
               <div className="col-md-12 mb-4">
-                Comments:{" "}
+                Review:{" "}
                 <textarea
-                  className="form-control"
+                  className={`form-control ${
+                    review.disable ? "" : review.feedback
+                  }`}
+                  disabled={review.disable}
                   rows="5"
-                  name="comments"
-                  value={comments}
+                  name="review"
+                  value={review.disable ? "" : review.field}
                   onChange={(e) => onChange(e)}
                 />
               </div>
+              <div className="col-md-12 mb-4">
+                Comments:{" "}
+                <textarea
+                  className={`form-control ${comments.feedback}`}
+                  rows="5"
+                  name="comments"
+                  value={comments.field}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+
               <div className="d-grid gap-2 mt-3">
                 <button type="submit" className="btn btn-primary">
                   Submit

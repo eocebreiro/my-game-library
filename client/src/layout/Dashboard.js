ffe208211ba4e399e777f0babea974784eec560f
Eric <eocebreiro@gmail.com>
2022-10-14 00:20:13 +0900

    moved some items as seprate components

--- client/src/layout/Dashboard.js
+++ client/src/layout/Dashboard.js
@@ -1,84 +1,43 @@
-import React from "react";
-import { TableRow } from "../components/TableRow";
+import React, { useEffect } from "react";
 
-const wishlistHeaders = ["Name", "Compilation", "System"];
-const backlockHeaders = ["Name", "Compilation", "System", "Ownership"];
+// State
+import { useUser } from "../contexts/UserContext";
 
-const libraryHeaders = [
-  "Name",
-  "Compilation",
-  "System",
-  "Status",
-  "Ownership",
-  "Hours",
-  "Rating",
-  "Review",
-  "Comments",
-];
-const content = [
-  {
-    _id: 1111111111,
-    name: "Witcher 3",
-    compilation: null,
-    system: "PS5",
-    status: "Wishlist",
-    ownership: "Owned",
-    hours: 102,
-    rating: 10,
-    review: null,
-    comments: null,
-  },
-  {
-    _id: 222222222,
-    name: "Spiderman 2",
-    compilation: "The DLC",
-    system: "PS4",
-    status: "Wishlist",
-    ownership: "Owned",
-    hours: 44,
-    rating: 7.5,
-    review: null,
-    comments: null,
-  },
-];
+// Actions
+import { getProfile } from "../contexts/UserActions";
+
+// Components
+import { GameLibrary } from "../components/Tables/GameLibrary";
+import { Backlog } from "../components/Tables/Backlog";
+import { Wishlist } from "../components/Tables/Wishlist";
+import { Filters } from "../components/Filters";
+import { DataChart } from "../components/DataChart";
 
 export const Dashboard = () => {
+  const { dispatch } = useUser();
+
+  useEffect(() => {
+    getProfile(dispatch);
+  }, []);
+
   return (
     <div className="container">
-      <div className="row gap-2">
-        <div className="card">
-          <div>Unfinished</div>
-          <div>Beaten</div>
-          <div>Completed</div>
-        </div>
-        <div className="card">
-          <div>Filter</div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <div>My Game Library</div>
-            <div> </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="row mb-4">
+            <DataChart />
           </div>
-          <div className="card-body"></div>
-          <div className="table-responsive">
-            <TableRow headers={libraryHeaders} content={content} />
+          <div className="row mb-4">
+            <Filters />
           </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">Backlog</div>
-          <div className="card-body"></div>
-          <div className="table-responsive">
-            <TableRow headers={backlockHeaders} content={content} />
+          <div className="row mb-4">
+            <GameLibrary />
           </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">Wishlist</div>
-          <div className="card-body">
-            <div className="table-responsive">
-              <TableRow headers={wishlistHeaders} content={content} />
-            </div>
+          <div className="row mb-4">
+            <Backlog />
+          </div>
+          <div className="row mb-4">
+            <Wishlist />
           </div>
         </div>
       </div>

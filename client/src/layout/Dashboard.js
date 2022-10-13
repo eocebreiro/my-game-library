8d412f178a2fea6876a395b53510a870f0132859
Eric C <eocebreiro@gmail.com>
2022-10-13 16:16:52 +0900

    added a table component (first commit)

--- client/src/layout/Dashboard.js
+++ client/src/layout/Dashboard.js
@@ -1,4 +1,46 @@
 import React from "react";
+import { TableRow } from "../components/TableRow";
+
+const wishlistHeaders = ["Name", "Compilation", "System"];
+const backlockHeaders = ["Name", "Compilation", "System", "Ownership"];
+
+const libraryHeaders = [
+  "Name",
+  "Compilation",
+  "System",
+  "Status",
+  "Ownership",
+  "Hours",
+  "Rating",
+  "Review",
+  "Comments",
+];
+const content = [
+  {
+    _id: 1111111111,
+    name: "Witcher 3",
+    compilation: null,
+    system: "PS5",
+    status: "Wishlist",
+    ownership: "Owned",
+    hours: 102,
+    rating: 10,
+    review: null,
+    comments: null,
+  },
+  {
+    _id: 222222222,
+    name: "Spiderman 2",
+    compilation: "The DLC",
+    system: "PS4",
+    status: "Wishlist",
+    ownership: "Owned",
+    hours: 44,
+    rating: 7.5,
+    review: null,
+    comments: null,
+  },
+];
 
 export const Dashboard = () => {
   return (
@@ -13,7 +55,31 @@ export const Dashboard = () => {
           <div>Filter</div>
         </div>
         <div className="card">
-          <div>LIST OF GAMES</div>
+          <div className="card-header">
+            <div>My Game Library</div>
+            <div> </div>
+          </div>
+          <div className="card-body"></div>
+          <div className="table-responsive">
+            <TableRow headers={libraryHeaders} content={content} />
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">Backlog</div>
+          <div className="card-body"></div>
+          <div className="table-responsive">
+            <TableRow headers={backlockHeaders} content={content} />
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">Wishlist</div>
+          <div className="card-body">
+            <div className="table-responsive">
+              <TableRow headers={wishlistHeaders} content={content} />
+            </div>
+          </div>
         </div>
       </div>
     </div>

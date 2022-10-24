751371f01c52db1bbca879e587b9484d75e2cd88
Eric <eocebreiro@gmail.com>
2022-10-24 23:20:45 +0900

    fixed bugs

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use(cors());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
+app.use("/favicon.ico", express.static("/img/favicon.ico"));
 app.use("/", (req, res) => res.send("Welcome to My Gaming Library"));
 
 // Error 404
